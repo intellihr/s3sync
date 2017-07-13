8de85f811b115ef57b25a1507569c3ca20ad34d2
Soloman Weng <soloman1124@gmail.com>
2017-07-13 14:40:27 +1000

    Bug fix

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
 
-if [ -n "$USE_ECS_TASK_IAM_ROLE" = "true" ]; then
+if [ "$USE_ECS_TASK_IAM_ROLE" = "true" ]; then
   AWS_CREDS=`curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI`
 fi
 
 if [ -n "$AWS_CREDS" ]; then
   export AWS_ACCESS_KEY_ID=`echo $AWS_CREDS | jq -r '.AccessKeyId'`
   export AWS_SECRET_ACCESS_KEY=`echo $AWS_CREDS | jq -r '.SecretAccessKey'`
+  export AWS_SESSION_TOKEN=`echo $AWS_CREDS | jq -r '.Token'`
 fi
 
 if [ -n "$CRON_SCHEDULE" ]; then
